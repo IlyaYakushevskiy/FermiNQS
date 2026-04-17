970b4364f505d8cf8b1b834a5685d0f8be35db3e
ILYA <ilyayakushevsky@gmail.com>
2026-04-17 21:33:11 +0200

    fix attilas fct

--- src/ansatz.py
+++ src/ansatz.py
@@ -90,8 +90,8 @@ class FermiSets(nnx.Module):
         self.rho_dense2 = nnx.Linear(in_features=hidden_units, out_features=1, rngs=rngs)
 
         ### Psi layer, combining symmetric and antisymmetric features
-        self.Psi_dense1 = nnx.Linear(in_features=hidden_units+ 3 , out_features=hidden_units+3, rngs=rngs) # +1 for Re{} and Im{} of the Log(nu)
-        self.Psi_dense2 = nnx.Linear(in_features=hidden_units+ 3 , out_features=2, rngs=rngs)
+        self.Psi_dense1 = nnx.Linear(in_features=hidden_units+ 2 , out_features=hidden_units+2, rngs=rngs) # +1 for Re{} and Im{} of the Log(nu)
+        self.Psi_dense2 = nnx.Linear(in_features=hidden_units+ 2 , out_features=2, rngs=rngs)
 
 
     def nu_antisymmetric(self, x): 
@@ -152,7 +152,18 @@ class FermiSets(nnx.Module):
                 r_test = (z[:, idx_i] - z[:, idx_j]) / jnp.sqrt(( jnp.square(jnp.abs(z[:, idx_i] - z[:, idx_j])) + 1.0**2))
                 y = jnp.prod(r_test, axis=1)
 
-                return y 
+                # a = 0.1
+                # z = x_reshaped[:, :, 0] + 1j * x_reshaped[:, :, 1]
+                # idx_i, idx_j = jnp.tril_indices(z.shape[1], k=-1)
+                # r_test = (z[:, idx_i] - z[:, idx_j]) / jnp.sqrt(( jnp.square(jnp.abs(z[:, idx_i] - z[:, idx_j])) + 1.0**2))
+                
+                # # raw product
+                # raw_prod = jnp.prod(r_test, axis=1)
+            
+                # epsilon = 1e-7 + 1e-7j
+                # y = jnp.log(raw_prod + epsilon)
+                return y
+
 
             else:
                 raise NotImplementedError
@@ -170,13 +181,13 @@ class FermiSets(nnx.Module):
         y = self.rho_dense1(y)
         y = nnx.gelu(y)
 
-        log_nu_real = jnp.real(nu)
-        log_nu_imag = jnp.imag(nu)
-        safe_real = jnp.clip(log_nu_real, min=-15.0, max=15.0)[:, None]
-        phase_cos = jnp.cos(log_nu_imag)[:, None]
-        phase_sin = jnp.sin(log_nu_imag)[:, None]
+        log_nu_real = jnp.real(nu)[:, None]
+        log_nu_imag = jnp.imag(nu)[:, None]
+        # safe_real = jnp.clip(log_nu_real, min=-15.0, max=15.0)[:, None]
+        # phase_cos = jnp.cos(log_nu_imag)[:, None]
+        # phase_sin = jnp.sin(log_nu_imag)[:, None]
 
-        log_feat_concat = jnp.concatenate([y, safe_real, phase_cos, phase_sin], axis=-1)
+        log_feat_concat = jnp.concatenate([y, log_nu_real, log_nu_imag], axis=-1)
 
         logPsi = self.Psi_dense1(log_feat_concat)
         logPsi = nnx.gelu(logPsi)
@@ -195,7 +206,7 @@ class FermiSets(nnx.Module):
 
         nu = self.nu_antisymmetric(x)
         log_psi0_plus = self.eval_psi0(x, nu) 
-        log_psi0_minus = self.eval_psi0(x, -nu ) # nu + 1j * jnp.pi is a swap ( nu -> -nu) in complex space 
+        log_psi0_minus = self.eval_psi0(x, -nu) # nu + 1j * jnp.pi is a swap ( nu -> -nu) in complex space 
 
         stacked_logs = jnp.stack([log_psi0_plus, log_psi0_minus], axis=-1).astype(jnp.complex128)
         weights = jnp.array([0.5, -0.5], dtype=jnp.complex128) # negative weight creates complex fct 
